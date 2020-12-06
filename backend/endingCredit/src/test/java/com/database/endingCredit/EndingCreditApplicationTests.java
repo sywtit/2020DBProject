c20461edfec41deef6dfad54d10b0a258a029dc1
sywtit <youworthit17@naver.com>
2020-12-06 23:37:41 +0900

    setting vue-spring boot

---
+++ backend/endingCredit/src/test/java/com/database/endingCredit/EndingCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.endingCredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
